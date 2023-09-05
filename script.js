73f90b0f313211766bd70fa6b3f5d530cdf69001
Atharvkasar04 <atharvk752@gmail.com>
2023-09-05 21:04:22 +0530

    fixed the issue

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ async function checkWeather(city){
     if(response.status == 404){
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
+        searchBox.value = "";
     }
     else{
         document.querySelector(".error").style.display = "none";
